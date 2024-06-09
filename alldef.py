755b8084abaf32e08b3c98ad40c2103a09feec10
lyw182 <lyw18269276200@outlook.com>
2024-06-09 14:21:44 +0800

    第二次更改，修改了部分方法 依然不可用ing

--- alldef.py
+++ alldef.py
@@ -4,8 +4,8 @@ import time
 import hashlib
 import urllib.parse
 import urllib.request
-import requests
 import os
+import requests
 
 mixinKeyEncTab = [
     46, 47, 18, 2, 53, 8, 23, 32, 15, 50, 10, 31, 58, 3, 45, 35, 27, 43, 5, 49,
@@ -23,13 +23,26 @@ def appsign(params, appkey, appsec):
     params.update({'sign':sign})
     return params
 
-def getMixinKey(orig: str):
+def get_mixinkey(orig: str):
     '对 imgKey 和 subKey 进行字符顺序打乱编码'
     return reduce(lambda s, i: s + orig[i], mixinKeyEncTab, '')[:32]
 
-def signWbi(params: dict, img_key: str, sub_key: str):
-    '为请求参数进行 wbi 签名'
-    mixin_key = getMixinKey(img_key + sub_key)
+def get_wbikeys(params:dict) -> dict:
+    '''为请求参数进行 Wbi 签名
+    @params:未签名参数
+    '''
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
+        'Referer': 'https://www.bilibili.com/'
+    }
+    resp = requests.get('https://api.bilibili.com/x/web-interface/nav', headers=headers,timeout=10)
+    resp.raise_for_status()
+    json_content = resp.json()
+    img_url: str = json_content['data']['wbi_img']['img_url']
+    sub_url: str = json_content['data']['wbi_img']['sub_url']
+    img_key = img_url.rsplit('/', 1)[1].split('.')[0]
+    sub_key = sub_url.rsplit('/', 1)[1].split('.')[0]
+    mixin_key = get_mixinkey(img_key + sub_key)
     curr_time = round(time.time())
     params['wts'] = curr_time                                   # 添加 wts 字段
     params = dict(sorted(params.items()))                       # 按照 key 重排参数
@@ -44,40 +57,35 @@ def signWbi(params: dict, img_key: str, sub_key: str):
     params['w_rid'] = wbi_sign
     return params
 
-def getWbiKeys() -> tuple[str, str]:
-    '获取最新的 img_key 和 sub_key'
+def get_info(bvid:str) -> dict:
+    '''获取视频信息
+    @bvid:视频BV号
+    '''
+    info_url = 'https://api.bilibili.com/x/web-interface/view?'
+    info_params = get_wbikeys({'bvid':bvid})
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
         'Referer': 'https://www.bilibili.com/'
     }
-    resp = requests.get('https://api.bilibili.com/x/web-interface/nav', headers=headers,timeout=10)
-    resp.raise_for_status()
-    json_content = resp.json()
-    img_url: str = json_content['data']['wbi_img']['img_url']
-    sub_url: str = json_content['data']['wbi_img']['sub_url']
-    img_key = img_url.rsplit('/', 1)[1].split('.')[0]
-    sub_key = sub_url.rsplit('/', 1)[1].split('.')[0]
-    return img_key, sub_key
-
-def getInfo(bvid:str) -> dict:
-    '获取视频信息'
-    info_url = 'https://api.bilibili.com/x/web-interface/view'
-    info_params = {'bvid':bvid}
-    info = requests.get(url=info_url,params=info_params,timeout=10)
-    if info.status_code == 0:
+    info = requests.get(url=info_url,headers=headers,params=info_params,timeout=10)
+    if info.status_code == 200:
         return info.json()
-    if info.status_code != 0:
+    if info.status_code != 200:
         return {'code':info.status_code}
 
-def getVideoUrl(bvid:str,cid:str,qn:str) -> str:
-    '获取视频url'
-    video_stream_url = 'https://api.bilibili.com/x/player/wbi/playurl'
-    video_stream_params = {'bvid':bvid,'cid':cid,'qn':qn,'platform':'html5','high_quality':'1'}
+def get_videourl(bvid:str,cid:str,qn:str) -> dict:
+    '''获取视频url
+    @bvid:视频BV号
+    @cid:视频cid，由get_info方法获取
+    @qn:视频清晰度，16:360P 流畅，32:480P 清晰，64:720P 高清
+    '''
+    video_stream_url = 'https://api.bilibili.com/x/player/wbi/playurl?'
+    video_stream_params = get_wbikeys({'bvid':bvid,'cid':cid,'qn':qn,'platform':'html5','high_quality':'1'})
     video_url = requests.get(url=video_stream_url,params=video_stream_params,timeout=10)
-    if video_url.status_code == 0:
-        return video_url.json()['data']['durl']['url']
-    if video_url.status_code != 0:
-        return video_url.status_code
+    if video_url.status_code == 200:
+        return video_url.json()
+    if video_url.status_code != 200:
+        return {'code':video_url}
 
 def percent(a:int,b:int,c:int) -> int:
     '''获取下载进度百分比
@@ -88,9 +96,9 @@ def percent(a:int,b:int,c:int) -> int:
     per = 100*a*b/c
     if per > 100:
         per = 100
-    print(per)
+    return per
 
-def getVideo(url:str,name:str,dir:str=os.path.abspath('.')) -> str:
+def get_video(url:str,name:str,dir:str=os.path.abspath('.')) -> str:
     '''下载视频'''
     filename = dir + name
     urllib.request.urlretrieve(url=url,filename=filename,reporthook=percent)
--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-from sys import exit
 from time import sleep
+import sys
 import alldef
+
+params = {
+    'id':436275,
+    'str':'48265113',
+    'test':'abcdefg',
+}
+appkey = "1d8b6e7d45233436"
+appsec = "560c52ccd288fed045859ed18bffd973"
 
 print('操作选项：\n1:下载视频')
 choice = input('请选择您想要执行的操作：')
@@ -8,19 +16,23 @@ bvid = input('请输入视频bv号：')
 qn = input('请输入你需要的分辨率：\n16:360P 流畅\n32:480P 清晰\n64:720P 高清')
 if choice == '1':
     print('获取视频信息中...')
-    video_info = alldef.getInfo(bvid=bvid)
-    print(video_info)
+    video_info = alldef.get_info(bvid=bvid)
     if video_info['code'] != 0:
         print('视频信息获取失败，错误码为' + str(video_info['code']))
         print('程序将在5秒后退出')
         sleep(5)
-        exit() 
+        sys.exit() 
     
     video_title = video_info['data']['title']
     video_cid = video_info['data']['cid']
     print('获取视频地址中...')
-    url = alldef.getVideoUrl(bvid=bvid,cid=video_cid,qn=qn)
+    url = alldef.get_videourl(bvid=bvid,cid=video_cid,qn=qn)
+    print(url)
+    if url['code'] != 0:
+        print('视频地址获取失败，错误码为' + str(url['code']))
+        print('程序将在5秒后退出')
+        sleep(5)
+        sys.exit() 
     print('文件下载中...')
-    filename = alldef.getVideo(url=url,name=video_title)
-    print('下载任务已结束')
-
+    filename = alldef.get_video(url=url['data']['durl']['url'],name=video_title)
+    print('下载任务已结束，文件为' + filename)
