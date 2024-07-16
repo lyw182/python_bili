81a34392a8eff44d17f055507651c2f3e134b758
Dogwen <lyw18269276200@outlook.com>
2024-07-16 09:21:49 +0800

    将requests改为使用httpx，并添加了搜索功能

--- alldef.py
+++ alldef.py
@@ -2,10 +2,9 @@ import hashlib
 import os
 import time
 import urllib.parse
+import re
 from functools import reduce
-
-import requests
-
+import httpx
 
 
 mixinKeyEncTab = [
@@ -16,14 +15,33 @@ mixinKeyEncTab = [
 ]
 
 
-def appsign(params, appkey, appsec):
-    """为请求参数进行 APP 签名"""
-    params.update({'appkey': appkey})
-    params = dict(sorted(params.items()))  # 按照 key 重排参数
-    query = urllib.parse.urlencode(params)  # 序列化参数
-    sign = hashlib.md5((query + appsec).encode()).hexdigest()  # 计算 api 签名
-    params.update({'sign': sign})
-    return params
+#def appsign(params, appkey, appsec):
+#    """为请求参数进行 APP 签名"""
+#    params.update({'appkey': appkey})
+#    params = dict(sorted(params.items()))  # 按照 key 重排参数
+#    query = urllib.parse.urlencode(params)  # 序列化参数
+#    sign = hashlib.md5((query + appsec).encode()).hexdigest()  # 计算 api 签名
+#    params.update({'sign': sign})
+#    return params
+
+def get_cookies():
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
+        'Referer': 'https://www.bilibili.com/'
+    }
+    cookies = httpx.get(url='https://www.bilibili.com/', headers=headers)
+    return cookies.cookies
+
+
+def get_search(keyword: str):
+    search_url = 'https://api.bilibili.com/x/web-interface/wbi/search/type'
+    params = {'keyword': keyword, 'search_type': 'video'}
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
+        'Referer': 'https://www.bilibili.com/'
+    }
+    search = httpx.get(url=search_url, headers=headers, cookies=get_cookies(), params=get_wbikeys(params=params))
+    return search.json()['data']['result']
 
 
 def get_mixinkey(orig: str):
@@ -39,7 +57,8 @@ def get_wbikeys(params: dict) -> dict:
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
         'Referer': 'https://www.bilibili.com/'
     }
-    resp = requests.get('https://api.bilibili.com/x/web-interface/nav', headers=headers, timeout=10)
+    wbikeys_url = 'https://api.bilibili.com/x/web-interface/nav'
+    resp = httpx.get(url=wbikeys_url, headers=headers, timeout=10)
     resp.raise_for_status()
     json_content = resp.json()
     img_url: str = json_content['data']['wbi_img']['img_url']
@@ -72,30 +91,26 @@ def get_info(bvid: str) -> dict:
     }
     info_url = 'https://api.bilibili.com/x/web-interface/view?'
     info_params = get_wbikeys({'bvid': bvid})
-    info = requests.get(url=info_url, headers=headers, params=info_params, timeout=10)
+    info = httpx.get(url=info_url, headers=headers, params=info_params, timeout=10)
     if info.status_code == 200:
         return info.json()
     if info.status_code != 200:
         return {'code': info.status_code}
 
 
-def get_videourl(bvid: str, cid: str, qn: str) -> dict:
+def get_videourl(bvid: str, cid: str) -> dict:
     """获取视频url
     @bvid:视频BV号
     @cid:视频cid，由get_info方法获取
-    @qn:视频清晰度，16:360P 流畅，32:480P 清晰，64:720P 高清
     """
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
         'Referer': 'https://www.bilibili.com/'
     }
     video_stream_url = 'https://api.bilibili.com/x/player/wbi/playurl?'
-    video_stream_params = get_wbikeys({'bvid': bvid, 'cid': cid, 'qn': qn})
-    video_url = requests.get(url=video_stream_url, params=video_stream_params, headers=headers, timeout=10)
-#    if video_url.status_code == 200:
+    video_stream_params = get_wbikeys({'bvid': bvid, 'cid': cid})
+    video_url = httpx.get(url=video_stream_url, params=video_stream_params, headers=headers, timeout=10)
     return video_url.json()
-#    if video_url.status_code != 200:
-#        return {'code': video_url}
 
 
 def download(url: str, name: str, output_path: str = os.path.abspath('.')) -> str:
@@ -108,10 +123,9 @@ def download(url: str, name: str, output_path: str = os.path.abspath('.')) -> st
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
         'Referer': 'https://www.bilibili.com/'
     }
-    chunk_size = 1024
-    filename = output_path + '\\' + name + '.mp4'
-    response = requests.get(url=url, headers=headers, stream=True)
-    with open(file=filename, mode="wb") as w:
-        for chunk in response.iter_content(chunk_size):
-            w.write(chunk)
+    filename = re.sub(pattern=r'/', repl=r' ', string=rf'{output_path}\{name}.mp4')
+    with httpx.stream(method='GET', url=url, headers=headers) as response:
+        with open(file=filename, mode='wb+') as w:
+            for chunk in response.iter_raw():
+                w.write(chunk)
     return filename
--- main.py
+++ main.py
@@ -10,28 +10,39 @@ params = {
 appkey = "1d8b6e7d45233436"
 appsec = "560c52ccd288fed045859ed18bffd973"
 
-print('操作选项：\n1:下载视频')
-choice = input('请选择您想要执行的操作：')
-bvid = input('请输入视频bv号：')
-qn = input('请输入你需要的分辨率：\n16:360P 流畅\n32:480P 清晰\n64:720P 高清')
-if choice == '1':
-    print('获取视频信息中...')
-    video_info = alldef.get_info(bvid=bvid)
-    if video_info['code'] != 0:
-        print('视频信息获取失败，错误码为' + str(video_info['code']))
-        print('程序将在5秒后退出')
-        time.sleep(5)
-        sys.exit()
 
-    video_title = video_info['data']['title']
-    video_cid = video_info['data']['cid']
-    print('获取视频地址中...')
-    url = alldef.get_videourl(bvid=bvid, cid=video_cid, qn=qn)
-    if url['code'] != 0:
-        print('视频地址获取失败，错误码为' + str(url['code']))
-        print('程序将在5秒后退出')
-        time.sleep(5)
-        sys.exit()
-    print('文件下载中...')
-    video = alldef.download(url=url['data']['durl'][0]['url'], name=video_title)
-    print('下载任务已结束，文件为' + video)
+print('操作选项：')
+print('1:搜索视频')
+print('2:下载视频')
+choice = input('请选择您想要执行的操作：')
+if choice == '1':
+    search_keyword = input('请输入搜索内容：')
+    search_response = alldef.get_search(keyword=search_keyword)
+    for i in range(0, len(search_response)-1):
+        print(search_response[i]['bvid'] + '   ' + search_response[i]['title'])
+elif choice == '2':
+    pass
+bvid = input('请输入视频bv号：')
+print('获取视频信息中...')
+video_info = alldef.get_info(bvid=bvid)
+if video_info['code'] != 0:
+    print('视频信息获取失败，错误码为' + str(video_info['code']))
+    print('程序将在5秒后退出')
+    time.sleep(5)
+    sys.exit()
+
+video_title = video_info['data']['title']
+video_cid = video_info['data']['cid']
+print('获取视频地址中...')
+url = alldef.get_videourl(bvid=bvid, cid=video_cid)
+if url['code'] != 0:
+    print('视频地址获取失败，错误码为' + str(url['code']))
+    print('程序将在5秒后退出')
+    time.sleep(5)
+    sys.exit()
+for qn_number in range(len(url['data']['accept_description'])):
+    print(str(url['data']['accept_quality'][qn_number]) + ':' + url['data']['accept_description'][qn_number])
+qn = input('请输入你需要的分辨率：')
+print('文件下载中...')
+video = alldef.download(url=url['data']['durl'][0]['url'], name=video_title)
+print('下载任务已结束，文件为' + video)
